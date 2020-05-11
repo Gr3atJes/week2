6c6f14524bbda4559e6c45a30f426d704dbc6632
Gr3atJes <mathiujes@gmail.com>
2020-05-12 02:52:54 +0300

    tweaked some javascript

--- js/jquery.js
+++ js/jquery.js
@@ -1,8 +1,7 @@
-
 var ysel = document.getElementsByName("year")[0],
   msel = document.getElementsByName("month")[0],
   dsel = document.getElementsByName("day")[0];
-for (var i = 2018; i >= 1900; i--) {
+for (var i = 2018; i >= 1985; i--) {
   var opt = new Option();
   opt.value = opt.text = i;
   ysel.add(opt);
@@ -10,52 +9,44 @@ for (var i = 2018; i >= 1900; i--) {
 ysel.addEventListener("change", validate_date);
 msel.addEventListener("change", validate_date);
 setInitialDays();
-jQuery("form.dayname-form").submit(function (
-  event
-) {
-  var fulldate =
-    jQuery(this).find("#month").val() +
-    "/" +
-    jQuery(this).find("#day").val() +
-    "/" +
-    jQuery(this).find("#year").val();
+$("form.dayname-form").submit(function (event) {
+  var fulldate = $(this).find("#month").val() + "/" + $(this).find("#day").val() + "/" + $(this).find("#year").val();
   //var fulldate = dsel+"/"+msel+"/"+ysel;
   console.log(fulldate);
 
-  jQuery(".dayname span").html(
-    DayName(
-      fulldate,
-      jQuery(this).find("#gender").val()
-    )
-  );
+  $(".dayname span").html(DayName(fulldate, $(this).find("#gender").val()));
   event.preventDefault();
 });
 
-// $(document).ready(function(e) {
-//     $('.gce-event-day .gce-feed').each(function() {
-//         var url = $.trim($(this).find("span:contains('Description:')").parent().find("a:last").html());
-//         var title =$.trim($(this).find(".gce-list-event").html());
-//         if (url) {
-//             $(this).find(".gce-list-event").html("<a class='gce-list-event' href='"+url+"' target='_blank'>"+title+"</a>");
 
-//         }
+function loadjs() {
 
-//     console.log('url: '+url);
-    
-//     });
+  $('ul#mobile_menu li.menu-item-has-children > a').append('<span class="mysub">3</span>');
+  $('.logo_container').prepend('<span id="et-info-phone"><a href="tel:+1 617 833 5207">617.833.5207</a></span>');
 
+  $('ul#mobile_menu li.menu-item-has-children > a').find('span.mysub').on("click", function (event) {
+    event.stopPropagation();
+    event.preventDefault();
 
+    $(this).closest('li').toggleClass('mymobile_sub_open');
 
-// });
+    if ($(this).closest('li').hasClass('mymobile_sub_open')) {
+      $(this).text('3');
+    }
+    else {
+      $(this).text('3');
+    }
 
+  });
+}
+window.onload = loadjs;
+$(document).ready(function(){
+  $('.page-id-9581 .gform_footer #gform_submit_button_21').removeClass('gform_button button');
+  $('.page-id-9581 .gform_footer #gform_submit_button_21').addClass('et_pb_promo_button et_pb_button');
+  $('.page-id-9581 .gform_footer #gform_save_21_link').removeClass('gform_save_link');
+  $('.page-id-9581 .gform_footer #gform_save_21_link').addClass('button_alter et_pb_promo_button et_pb_button');
+  });
 
-
-// function validateDate() { 
-//     var y = +ysel.value, m =msel.value, d = dsel.value;
-//     if(m === "2")
-//     var mlength = 28 + (!(y&3) && ((y % 100) !== 0 || !(y &)))
-// }
-    
-
-
-    
+  $(document).ready(function(){
+    $('.userpro-profile-img.userpro-force-center a').removeAttr('href');
+    });
--- js/scripts.js
+++ js/scripts.js
@@ -1,132 +1,89 @@
-var maleAkan = ["Kwasi", "Kwadwo","Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
-toString.maleAkan();
-var femaleAkan =["Akosua","Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
-toString.femaleAkan();
-var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-toString.days();
-var months = ["January","Feburary","March","April","May","June","July","August","September","October","November","December"]
-toString.months();
-var dt = newDate (dt);
-
-
-
-
-
-
-// JavaScript Document
-
-jQuery(document).ready(function(e) {
-	
-    // rewrite google calendar event items
-    jQuery('.gce-event-day .gce-feed').each(function() {
-        // add URL link to event title
-        var url = jQuery.trim(jQuery(this).find("span:contains('Description:')").parent().find("a:last").html());
-        var title = jQuery.trim(jQuery(this).find(".gce-list-event").html());
-        if (url) {
-                jQuery(this).find(".gce-list-event").html("<a class='gce-list-event' href='"+url+"' target='_blank'>"+title+"</a>");
-                //console.log(title+': '+url)
-        }
-
-        console.log('url: '+url);
-    });
   
-  
-  
+let akanForm = document.getElementById("akanForm");
+akanForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    gender = document.getElementById("gender").value
+    dateOfBirth = document.getElementById("date").value
+
+    dateOfBirth = new Date(dateOfBirth);
+
+    let yearOfBirth = dateOfBirth.getFullYear();
+    let birthMonth = dateOfBirth.getMonth() + 1;
+    let dayOfBirth = dateOfBirth.getDate();
+
+    CC = yearOfBirth.toString().substr(0, 2);
+    YY = yearOfBirth.toString().substr(2, 2);
+    MM = birthMonth
+    DD = dayOfBirth
+
+    dayOfTheWeek = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
+
+    dayOfTheWeek = parseInt(dayOfTheWeek);
+
+    let akanName;
+
+    femaleAkanNames = ["Ama", "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua"];
+    maleAkanNames = ["Kwame", "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi"];
+
+    let bornDay;
+
+    if (dayOfTheWeek == 0 && gender == "female") {
+        bornDay = "Saturday";
+        akanName = femaleAkanNames[0];
+    } else if (dayOfTheWeek == 0 && gender == "male") {
+        bornDay = "Saturday";
+        akanName = maleAkanNames[0];
+
+
+    } else if (dayOfTheWeek == 1 && gender == "female") {
+        bornDay = "Sunday";
+        akanName = femaleAkanNames[1];
+    } else if (dayOfTheWeek == 1 && gender == "male") {
+        bornDay = "Sunday";
+        akanName = maleAkanNames[1];
+
+
+    } else if (dayOfTheWeek == 2 && gender == "female") {
+        bornDay = "Monday";
+        akanName = femaleAkanNames[2];
+    } else if (dayOfTheWeek == 2 && gender == "male") {
+        bornDay = "Monday";
+        akanName = maleAkanNames[2];
+
+
+    } else if (dayOfTheWeek == 3 && gender == "female") {
+        bornDay = "Tuesday";
+        akanName = femaleAkanNames[3];
+    } else if (dayOfTheWeek == 3 && gender == "male") {
+        bornDay = "Tuesday";
+        akanName = maleAkanNames[3];
+
+
+    } else if (dayOfTheWeek == 4 && gender == "female") {
+        bornDay = "Wednesday";
+        akanName = femaleAkanNames[4];
+    } else if (dayOfTheWeek == 4 && gender == "male") {
+        bornDay = "Wednesday";
+        akanName = maleAkanNames[4];
+
+
+    } else if (dayOfTheWeek == 5 && gender == "female") {
+        bornDay = "Thursday";
+        akanName = femaleAkanNames[5];
+    } else if (dayOfTheWeek == 5 && gender == "male") {
+        bornDay = "Thursday";
+        akanName = maleAkanNames[5];
+
+
+    } else if (dayOfTheWeek == 6 && gender == "female") {
+        bornDay = "Friday";
+        akanName = femaleAkanNames[6];
+    } else if (dayOfTheWeek == 6 && gender == "male") {
+        bornDay = "Friday";
+        akanName = maleAkanNames[6];
+    }
+
+    let result = document.getElementById("reveal-result").innerHTML = "You were born on a " + bornDay + " so your akan name is " + akanName + "!"
+    document.getElementById("akanForm").reset();
 });
-
-// day name generator functions
-function validate_date() {
-    var y = +ysel.value, m = msel.value, d = dsel.value;
-    if (m === "2")
-        var mlength = 28 + (!(y & 3) && ((y % 100) !== 0 || !(y & 15)));
-    else var mlength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][m - 1];
-    dsel.length = 0;
-    for (var i = 1; i <= mlength; i++) {
-        var opt = new Option();
-        opt.value = opt.text = i;
-        if (i == d) opt.selected = true;
-        dsel.add(opt);
-    }
-}
-function setInitialDays() {
-	d = dsel.value;
-	for (var i = 1; i <= 31; i++) {
-        var opt = new Option();
-        opt.value = opt.text = i;
-        if (i == d) opt.selected = true;
-        dsel.add(opt);
-    }
-}
-function DayName(dt,gender) {
-    // set names to days
-    // supply birth date in field (MM/DD/YY)
-    // convert to date format; getDay from date; display
-    var name = ''; dayofweek = ''; var audio = '';
-	var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-	var days_abbr = ['Sun','Mon','Tues','Wed','Thu','Fri','Sat'];
-    var female_names = ['Akosua','Adjoa','Abenaa','Akua','Yaa','Afia','Amma'];
-    var male_names = ['Kwasi','Kodjo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
-    var dt = new Date(dt);
-    var day = dt.getDay();
-	if (day != null) {
-		 if (gender == 'male') {
-			name = male_names[day];
-		}
-		else {
-			name = female_names[day];
-		}
-		var gender_day = gender+'-'+days[day].toLowerCase();
-		console.log(gender_day);
-    	return '<p>Your day name is <strong>'+name+'</strong>, which means you were born on a <strong>'+days[day]+'</strong>.</p><div><strong>Listen:</strong> '+audio_output+'</div>';
-	}
-	else {
-		return 'You did not select a proper date. Please try again.';	
-    }
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-	var ysel = document.getElementsByName("year")[0],
-    msel = document.getElementsByName("month")[0],
-    dsel = document.getElementsByName("day")[0];
-	for (var i = 2018; i >= 1900; i--) {
-		var opt = new Option();
-		opt.value = opt.text = i;
-		ysel.add(opt);
-	}
-	ysel.addEventListener("change", validate_date);
-	msel.addEventListener("change", validate_date);
-	setInitialDays();
-	jQuery("form.dayname-form").submit(function (event) {
-		var fulldate = jQuery(this).find("#month").val()+"/"+jQuery(this).find("#day").val()+"/"+jQuery(this).find("#year").val();
-		//var fulldate = dsel+"/"+msel+"/"+ysel;
-		console.log(fulldate);
-		
-		jQuery(".dayname span").html(DayName(fulldate,jQuery(this).find("#gender").val()));		
-		event.preventDefault();
-	});
-	
-   
-}
