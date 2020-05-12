edd8364870a5c1f46e39fb48226ebaba2cf84ce8
Gr3atJes <mathiujes@gmail.com>
2020-05-12 08:12:45 +0300

    complete revamp

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,78 @@
-var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
-var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
-var weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
 
-//function calling
-function getName() {
-    var century = parseInt(document.getElementById("CENTURY").value);
-    var year = parseInt(document.getElementById("YEAR").value);
-    var month = parseInt(document.getElementById("MONTH").value);
-    var day = parseInt(document.getElementById("DAY").value);
-    var male = document.getElementById("MALE");
-    var female = document.getElementById("FEMALE");
 
+// JavaScript Document
+
+jQuery(document).ready(function(e) {
+   
+   // rewrite google calendar event items
+   jQuery('.gce-event-day .gce-feed').each(function() {
+       // add URL link to event title
+       var url = jQuery.trim(jQuery(this).find("span:contains('Description:')").parent().find("a:last").html());
+       var title = jQuery.trim(jQuery(this).find(".gce-list-event").html());
+       if (url) {
+               jQuery(this).find(".gce-list-event").html("<a class='gce-list-event' href='"+url+"' target='_blank'>"+title+"</a>");
+               //console.log(title+': '+url)
+       }
+
+       console.log('url: '+url);
+   });
+ 
+ 
+ 
+});
+
+// day name generator functions
+function validate_date() {
+   var y = +ysel.value, m = msel.value, d = dsel.value;
+   if (m === "2")
+       var mlength = 28 + (!(y & 3) && ((y % 100) !== 0 || !(y & 15)));
+   else var mlength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][m - 1];
+   dsel.length = 0;
+   for (var i = 1; i <= mlength; i++) {
+       var opt = new Option();
+       opt.value = opt.text = i;
+       if (i == d) opt.selected = true;
+       dsel.add(opt);
+   }
 }
-//validation
-if (day <= 0 || day > 31)
-    alert("invalid option please enter a valid date");
-else if (MONTH <= 0 || MONTH > 12 || MONTH == 2 && DAY > 29)
-    alert("invalid option please enter a valid date");
-
-    var day = new Date(CENTURY + "/" +YEAR + "/" + MONTH+ "/" +DAY );
-    var birthDay=day.getDay();
-    if(male.checked==true){
-       alert("You were born on " +weekDay[birthDay]+ " and Your Akan name is " + maleNames[birthDay])
-    }
-    else if(female.checked==true){
-       alert("You were born on " +weekDay[birthDay]+ " and Your Akan name is " + femaleNames[birthDay])
-    }
-    
+function setInitialDays() {
+   d = dsel.value;
+   for (var i = 1; i <= 31; i++) {
+       var opt = new Option();
+       opt.value = opt.text = i;
+       if (i == d) opt.selected = true;
+       dsel.add(opt);
+   }
+}
+function DayName(dt,gender) {
+   // set names to days
+   // supply birth date in field (MM/DD/YY)
+   // convert to date format; getDay from date; display
+   var name = ''; dayofweek = ''; var audio = '';
+   var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+   var days_abbr = ['Sun','Mon','Tues','Wed','Thu','Fri','Sat'];
+   var female_names = ['Akosua','Adjoa','Abenaa','Akua','Yaa','Afia','Amma'];
+   var male_names = ['Kwasi','Kodjo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
+   var dt = new Date(dt);
+   var day = dt.getDay();
+   if (day != null) {
+        if (gender == 'male') {
+           name = male_names[day];
+       }
+       else {
+           name = female_names[day];
+       }
+       var gender_day = gender+'-'+days[day].toLowerCase();
+       var audio_output = jQuery('.custom-table').find('.'+gender_day).html();
+       console.log(gender_day);
+       return '<p>Your day name is <strong>'+name+'</strong>, which means you were born on a <strong>'+days[day]+'</strong>.</p><div><strong>Listen:</strong> '+audio_output+'</div>';
+   }
+   else {
+       return 'You did not select a proper date. Please try again.';	
+   }
+  
+}
